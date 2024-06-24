756e67f28b9d7698c4fbf3443d70076050e7b8db
hntung <hongoctungii@gmail.com>
2024-06-24 19:06:26 +0700

    First custom components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,41 +1,12 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
+import { View } from 'react-native';
 import products from '@/assets/data/products';
+import ProductListItem from '@/src/components/ProductListItem';
 
-const product = products[1];
-
-export default function TabOneScreen() {
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[5]}/>
+      <ProductListItem product={products[6]}/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  }
-});
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
+import products from '@/assets/data/products';
+
+
+const ProductListItem = ({ product }: { product: any }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{uri: product.image}} style={styles.image}/>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  }
+});
