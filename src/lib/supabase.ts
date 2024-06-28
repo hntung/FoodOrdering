69efe4839339468d0d31f2813a28c61f91f0141b
hntung <hongoctungii@gmail.com>
2024-06-28 09:39:57 +0700

    Add supabase, update sign in and sign up

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://kqwjhlrzulxexaxhepjn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxd2pobHJ6dWx4ZXhheGhlcGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1Mzg4MDYsImV4cCI6MjAzNTExNDgwNn0.l9Pb2oVY9sSrvGKr_ZxDAkbQmdIx7DyVxNfHvY5j3EM';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
